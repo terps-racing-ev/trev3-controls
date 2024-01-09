e1f45df737be52239fb35ba02296944a40166c1a
Yasadu De Silva <ydesilva@terpmail.umd.edu>
2024-01-08 19:06:53 -0500

    restart
    restart programming everything from scratch
    here, test that CAN output works

--- src/main.c
+++ src/main.c
@@ -52,7 +52,7 @@
  * Controls Settings
  ***************************************************************************/
 
-/* min. difference between the APPS pct travels 
+/* min. difference between the APPS pct travels
  * that'll trigger the implausibility check */
 #define APPS_MIN_IMPLAUSIBLE_DEVIATION 10
 
@@ -67,7 +67,7 @@
  * Controls Math Macros
  ***************************************************************************/
 
-/* TODO: voltage->pct travel code*/ 
+/* TODO: voltage->pct travel code*/
 #define VoltageToPctTravelApps1(val) (0)
 #define VoltageToPctTravelApps2(val) (0)
 
@@ -81,16 +81,19 @@
 
 #define CAN_CHANNEL IO_CAN_CHANNEL_0
 
-#define BAUD_RATE 250
+#define BAUD_RATE 500
 
 /* The system sends CAN messages from a FIFO buffer, this constant defines
  * its size. If a message is added to the full buffer, it is ignored.
  */
 #define FIFO_BUFFER_SIZE 20
 
-#define VCU_CAN_ID 0
+#define VCU_CAN_ID 193
 
-
+/**************************************************************************
+ * Other
+ ***************************************************************************/
+ #define CYCLE_TIME_MS 5
 
 /* Application Database,
  * needed for TTC-Downloader
@@ -131,6 +134,8 @@ void main (void)
     ubyte4 timestamp;
     ubyte4 implausibility_timestamp;
 
+    enum VCU_State { NOT_READY, PLAYING_RTD_SOUND, DRIVING, ERRORED };
+
     /*******************************************/
     /*             INITIALIZATION              */
     /*******************************************/
@@ -145,79 +150,35 @@ void main (void)
     /* NOTE: turns 5v sensor supply 1 on */
     IO_POWER_Set (IO_ADC_SENSOR_SUPPLY_0, IO_POWER_ON);
 
-    /* APPS 1 */
-    bool adc_fresh_apps_1;
-    ubyte2 adc_val_apps_1;
-    IO_ADC_ChannelInit( IO_PIN_APPS_1
-                      , IO_ADC_ABSOLUTE
-                      , 0
-                      , 0
-                      , 0
-                      , NULL );
-
-    /* APPS 2 */
-    bool adc_fresh_apps_2;
-    ubyte2 adc_val_apps_2;
-    IO_ADC_ChannelInit( IO_PIN_APPS_2
-                      , IO_ADC_ABSOLUTE
-                      , 0
-                      , 0
-                      , 0
-                      , NULL );
-
-    /* BSE */
-    bool adc_fresh_bse;
-    ubyte2 adc_val_bse;
-    IO_ADC_ChannelInit( IO_PIN_BSE
-                      , IO_ADC_ABSOLUTE
-                      , 0
-                      , 0
-                      , 0
-                      , NULL );
-
-    /* RTD */
-    bool di_val_rtd;
-    IO_DI_Init( IO_PIN_RTD
-    		  , IO_DI_PU_10K );
-
-    bool ready_to_drive = FALSE;
-    
-    /* APPS variables */
-    bool implausible_deviation = FALSE;
-    int diff = 0;
-    int pct_travel_apps_1 = 0;
-    int pct_travel_apps_2 = 0;
-    int avg_pct_travel = 0;
-    int torque_to_send = 0;
-    
-    bool outside_max_or_min = FALSE;
-    
     /* CAN variables and initialization */
     ubyte1 handle_fifo_w;
-    
+
     IO_CAN_DATA_FRAME can_frame[1] = {{{0}}};
-    
+
     IO_CAN_Init( CAN_CHANNEL
                , BAUD_RATE
                , 0
                , 0
                , 0);
-    
+
     IO_CAN_ConfigFIFO( &handle_fifo_w
     				 , CAN_CHANNEL
     				 , FIFO_BUFFER_SIZE
-    				 , IO_CAN_MSG_WRITE 
-    				 , IO_CAN_STD_FRAME 
-    				 , VCU_CAN_ID 
+    				 , IO_CAN_MSG_WRITE
+    				 , IO_CAN_STD_FRAME
+    				 , VCU_CAN_ID
     				 , 0);
-    
+
+
+    enum VCU_State current_state = NOT_READY;
+
 
     /*******************************************/
     /*       PERIODIC APPLICATION CODE         */
     /*******************************************/
 
     /* main loop, executed periodically with a
-     * defined cycle time (here: 10 ms)
+     * defined cycle time (here: CYCLE_TIME_MS ms)
      */
     while (1)
     {
@@ -229,118 +190,19 @@ void main (void)
          */
         IO_Driver_TaskBegin();
 
-        if (!ready_to_drive) {
-        	/* retrieve values for RTD switch and BSE */
-        	IO_DI_Get(IO_PIN_RTD, &di_val_rtd);
-        	IO_ADC_Get(IO_PIN_BSE, &adc_val_bse, &adc_fresh_bse);
-        	
-        	/* if brakes are pressed and RTD is activated, activate
-        	 * ready-to-drive
-        	 */
-        	if (di_val_rtd == TRUE && adc_val_bse >= BSE_THRESHOLD_RTD) {
-        		ready_to_drive = TRUE;
-        		
-        		/*
-        		 * TODO: RTD BUZZER CODE
-        		 */
-        	}
+        can_frame[0].id = VCU_CAN_ID;
+        can_frame[0].id_format = IO_CAN_STD_FRAME;
+        can_frame[0].length = 8;
+        can_frame[0].data[0] = current_state;
+        can_frame[0].data[1] = 0;
+        can_frame[0].data[2] = 0;
+        can_frame[0].data[3] = 0;
+        can_frame[0].data[4] = 1;
+        can_frame[0].data[5] = 0;
+        can_frame[0].data[6] = 0;
+        can_frame[0].data[7] = 0;
 
-        } else {
-        	/* check if the RTD switch is on */
-        	IO_DI_Get(IO_PIN_RTD, &di_val_rtd);
-
-        	if (di_val_rtd == TRUE) {
-        		/* get the raw values for both APPS sensors */
-                IO_ADC_Get(IO_PIN_APPS_1, &adc_val_apps_1, &adc_fresh_apps_1);
-                IO_ADC_Get(IO_PIN_APPS_2, &adc_val_apps_2, &adc_fresh_apps_2);
-                
-                /* calculate pct travel */
-                if (adc_fresh_apps_1 && adc_fresh_apps_2) {
-                	pct_travel_apps_1 = VoltageToPctTravelApps1(adc_val_apps_1);
-                	pct_travel_apps_2 = VoltageToPctTravelApps2(adc_val_apps_2);
-                }
-                
-                /* get the difference between the two */
-                diff = pct_travel_apps_2 - pct_travel_apps_1;
-                diff = Abs(diff);
-                
-                /* if the difference is above the implausibility requirement */
-                if (diff >= APPS_MIN_IMPLAUSIBLE_DEVIATION) {
-                	IO_RTC_StartTime(&implausibility_timestamp);
-                	
-                	implausible_deviation = TRUE;
-                	
-                	/* check if the implausiblity persists for 100 msec */
-                	while (IO_RTC_GetTimeUS(implausibility_timestamp) 
-                			< IMPLAUSIBILITY_PERSISTENCE_PERIOD_US
-                			&& implausible_deviation) {
-                		
-                		/* keep checking the difference between the 2 apps's */
-                        IO_ADC_Get(IO_PIN_APPS_1, &adc_val_apps_1, &adc_fresh_apps_1);
-                        IO_ADC_Get(IO_PIN_APPS_2, &adc_val_apps_2, &adc_fresh_apps_2);
-                        
-                        if (adc_fresh_apps_1 && adc_fresh_apps_2) {
-                        	pct_travel_apps_1 = VoltageToPctTravelApps1(adc_val_apps_1);
-                        	pct_travel_apps_2 = VoltageToPctTravelApps2(adc_val_apps_2);
-                        }
-                        
-                        diff = pct_travel_apps_2 - pct_travel_apps_1;
-                        diff = Abs(diff);
-                        
-                        /* stop checking once the 2 values don't deviate anymore */
-                        if (diff < APPS_MIN_IMPLAUSIBLE_DEVIATION) {
-                        	implausible_deviation = FALSE;
-                        }
-                	}
-
-                }
-            	
-                /* check if either APPS has a value outside the max or min
-                 * (short circuit)
-                 */
-                if (pct_travel_apps_2 > MAX_TRAVEL || pct_travel_apps_2 < MIN_TRAVEL
-                	|| pct_travel_apps_1 > MAX_TRAVEL || pct_travel_apps_1 < MIN_TRAVEL) {
-                	outside_max_or_min = TRUE;
-                }
-                
-                /* calculate the torque to be sent */
-            	if (implausible_deviation || outside_max_or_min) {
-            		torque_to_send = 0;
-            		/* make it so that RTD has to be flicked off and on before 
-            		 * the car drives again
-            		 */
-            		ready_to_drive = FALSE;
-            		
-            		/* reset the error checks */
-            		implausible_deviation = FALSE;
-            		outside_max_or_min = FALSE;
-            	} else {
-            		avg_pct_travel = ((pct_travel_apps_2 + pct_travel_apps_1) / 2);
-            		torque_to_send = VoltageToTorque(avg_pct_travel);
-            	}
-            	
-            	can_frame[0].id = VCU_CAN_ID;
-            	can_frame[0].id_format = IO_CAN_STD_FRAME;
-            	can_frame[0].length = 8;
-            	can_frame[0].data[0] = (torque_to_send * 10) % 256;
-            	can_frame[0].data[1] = (torque_to_send * 10) / 256;
-            	can_frame[0].data[2] = 0;
-            	can_frame[0].data[3] = 0;
-            	can_frame[0].data[4] = 1;
-            	can_frame[0].data[5] = 0;
-            	can_frame[0].data[6] = 0;
-            	can_frame[0].data[7] = 0;
-            	
-            	IO_CAN_WriteFIFO(handle_fifo_w, can_frame, 1);
-
-        	} else {
-        		/* If the RTD switch has been turned off, the RTD procedure
-        		 * has to be repeated
-        		 */
-        		ready_to_drive = FALSE;
-        	}
-        }
-
+        IO_CAN_WriteFIFO(handle_fifo_w, can_frame, 1);
 
         /* Task end function for IO Driver
          * This function needs to be called at
@@ -354,6 +216,6 @@ void main (void)
          * 10 milliseconds, this code delays the end of the cycle so it lasts
          * 10 milliseconds
          */
-        while (IO_RTC_GetTimeUS(timestamp) < MsToUs(10));
+        while (IO_RTC_GetTimeUS(timestamp) < MsToUs(CYCLE_TIME_MS));
     }
 }
