4fb725921a3f69ac8820dc51fed609e9df6c08bc
unknown <alinadandrade@outook.com>
2025-07-10 21:17:43 -0400

    sync

--- src/launch_control.h
+++ src/launch_control.h
@@ -3,13 +3,13 @@
 
 #include "APDB.h"
 
-#define LAUNCH_CONTROL_KP 10.0
+#define LAUNCH_CONTROL_KP 5.0
 #define LAUNCH_CONTROL_KI 0.0
 #define LAUNCH_CONTROL_KD 0.0
 
 #define LAUNCH_CONTROL_TARGET_SLIP_RATIO 1.1
 
-#define MINIMUM_TORQUE_LIMIT 10 // minimum torque limit for launch control, in Nm
+#define MINIMUM_TORQUE_LIMIT 110 // minimum torque limit for launch control, in Nm
 
 
 ubyte2 get_launch_control_torque_limit(float4 torque, float4 wheel_slip);
